faf83fd12e323a404cb3accdc786bb56799c13b1
Kenny <theokenny@hotmail.com>
2019-07-28 21:57:04 +1000

    Removed default file and include default task into index

--- gulpfile.babel.js/default.js
+++
@@ -1,5 +0,0 @@
-import gulp from 'gulp';
-import watch from './watch';
-
-//gulp.series is the same as gulp-sequence. New feature of version 4.0
-gulp.task('default', gulp.series(watch));
--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,6 +1,5 @@
-import './default';
-
 import gulp from 'gulp';
 import watch from './watch';
 
 gulp.task('watch', watch);
+gulp.task('default', gulp.series(watch));
